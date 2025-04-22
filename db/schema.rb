33a1b45fd498ad2556fca57be24d019ae35767df
yumachin <curryuma925@gmail.com>
2025-04-22 20:58:39 +0900

    Gemfile.lockとschema.rbを更新し、プラットフォーム固有の依存関係を追加

--- db/schema.rb
+++ db/schema.rb
@@ -82,8 +82,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_29_124205) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "name"
-    t.string "email"
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
