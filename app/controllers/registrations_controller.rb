42e8b8dac8787c145902405346583dea7aa08ea6
yumachin <curryuma925@gmail.com>
2025-06-07 13:20:38 +0900

    Set cookie expiration to 1 day and add database configuration for production

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,11 @@ class RegistrationsController < DeviseTokenAuth::RegistrationsController
       token = user.create_new_auth_token
 
       response.set_header("Set-Cookie", [
-        "access-token=#{token['access-token']}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=60",
-        "client=#{token['client']}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=60",
-        "email=#{token['uid']}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=60",
-        "uid=#{user.id}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=60",
-        "token-type=Bearer; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=60"
+        "access-token=#{token['access-token']}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=86400",
+        "client=#{token['client']}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=86400",
+        "email=#{token['uid']}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=86400",
+        "uid=#{user.id}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=86400",
+        "token-type=Bearer; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=86400"
       ])
       @current_user = user
       render json: { data: user }, status: :ok
